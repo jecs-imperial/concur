9d9e709644eab1cf4efbc61590b2943710cd12fd
James Smith <james.smith@openmathematics.org>
2018-10-17 14:25:40 +0100

    Added a locale agnostic string comparison function.

--- es6/stringCompare.js
+++ es6/stringCompare.js
@@ -1,7 +1,33 @@
 'use strict';
 
 function stringCompare(stringA, stringB) {
-  return (stringA.localeCompare(stringB) < 0)
+  if ((stringA === '') && (stringB === '')) {
+    return false;
+  }
+
+  if (stringA === '') {
+    return true;
+  }
+
+  if (stringB === '') {
+    return false;
+  }
+
+  const codePointA = stringA.codePointAt(0),
+        codePointB = stringB.codePointAt(0);
+
+  if (codePointA < codePointB) {
+    return true;
+  }
+
+  if (codePointA > codePointB) {
+    return false;
+  }
+
+  const subStringA = stringA.substring(1),
+        subStringB = stringB.substring(1);
+
+  return stringCompare(subStringA, subStringB);
 }
 
 module.exports = stringCompare;
