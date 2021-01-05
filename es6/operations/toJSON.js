d65b3a707e7acac3bbfdf055c0e27ce1e5001629
James Smith <james.smith@openmathematics.org>
2021-01-05 20:18:03 +0000

    Moved the sufficient dependeny into the developer dependencies.

--- es6/operations/toJSON.js
+++ es6/operations/toJSON.js
@@ -1,7 +1,5 @@
 "use strict";
 
 export default function operationsToJSON(operations) {
-  return operations.map((operation) => {
-    return operation.toJSON();
-  });
+  return operations.map((operation) => operation.toJSON());
 }
